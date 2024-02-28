ac3678cd79ad3f266e1d089275066242cc9e83a0
Christian Valeri <mortlaxris@outlook.com>
2024-02-27 19:29:14 -0600

    added code that counts up the amount of waldos found

--- js/waldo1.js
+++ js/waldo1.js
@@ -7,7 +7,19 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 waldo1.get("/waldo1", (req, res) => {
-    res.render('waldo1');
+    res.render('waldo1', { waldo: waldo });
+});
+
+let waldo = 0;
+waldo1.post("/waldo1", async (req, res, next) => {
+    if (waldo < 5) {
+        waldo += 1;
+        res.redirect("waldo1");
+    } else {
+        res.status(204).send();//Keeps user on same page
+    }
+
+    console.log("Waldo button clicked times: " + waldo);
 });
 
 module.exports = waldo1;
