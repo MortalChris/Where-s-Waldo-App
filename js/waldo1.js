b41bd041e3a10f09bc39042c0c6324e49476dbcf
Christian Valeri <mortlaxris@outlook.com>
2024-03-06 19:35:34 -0600

    added commented out code for waldo items

--- js/waldo1.js
+++ js/waldo1.js
@@ -10,9 +10,19 @@ waldo1.get("/waldo1", (req, res) => {
     res.render('waldo1', { waldo: waldo });
 });
 
+// let item = 0;
 let waldo = 0;
+
 waldo1.post("/waldo1", async (req, res, next) => {
-    if (waldo < 5) {
+    //If you find items adds to tally
+    // if (item < 5) {
+    //     item += 1;
+    //     res.redirect("waldo1");
+    // } else {
+    //     res.status(204).send();//Keeps user on same page
+    // }
+    //If you find waldos adds to tally
+    if (waldo < 3) {
         waldo += 1;
         res.redirect("waldo1");
     } else {
